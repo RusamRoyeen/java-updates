7d55946bd26c07234a1aec81bfb4360dc4ad3cc3
Rustam Royeen <rustam.royeen@gmail.com>
2024-06-23 17:42:46 -0400

    m2-Enum created

---
+++ m2-enum/src/main/java/Operation.java
@@ -0,0 +1,9 @@
+public enum Operation {
+
+    PLUS, MINUS, MULTIPLY, DIVIDE; // each constant is object(new) of Operation
+
+
+    Operation() {
+        System.out.println("Constructor");
+    }
+}
---
+++ m2-enum/src/main/java/ProjectStatus.java
@@ -0,0 +1,13 @@
+public enum ProjectStatus {
+
+    OPEN, CLOSED, PROGRESS("IN PROGRESS");
+
+    String status;
+
+    ProjectStatus(String status) {
+        this.status = status;
+    }
+
+    ProjectStatus() {
+    }
+}
